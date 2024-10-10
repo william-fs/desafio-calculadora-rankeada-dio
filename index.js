bd7f52f3e76f9ff67a40c652307df9ee9dbac8ca
William Ferreira <wfs19@live.com>
2024-10-10 19:59:39 -0300

    Solução do desafio

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Calculadora Rankeada - DIO
+
+function calculoRankeada(qtdVitorias, qtdDerrotas) {
+    let saldoVitorias = qtdVitorias - qtdDerrotas;
+    let classificacao;
+
+    if (saldoVitorias <= 10) {
+        classificacao = "Ferro";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        classificacao = "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        classificacao = "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        classificacao = "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        classificacao = "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        classificacao = "Lendário";
+    } else if (saldoVitorias >= 101) {
+        classificacao = "Imortal";
+    }
+
+    return `O Herói tem de saldo de vitórias ${saldoVitorias} e está no nível de ${classificacao}`;
+}
+
+console.log(calculoRankeada(110, 100));
+console.log(calculoRankeada(110, 90));
+console.log(calculoRankeada(110, 60));
+console.log(calculoRankeada(110, 59));
+console.log(calculoRankeada(110, 27));
+console.log(calculoRankeada(110, 12));
+console.log(calculoRankeada(110, 2));
